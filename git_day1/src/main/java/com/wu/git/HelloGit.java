c7810b4659013ca970e6bf9380bb939806133528
wu <wuyi1619@qq.com>
2022-01-26 14:13:29 +0800

    添加文件

---
+++ git_day1/src/main/java/com/wu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.wu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-01-26 13:57
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
